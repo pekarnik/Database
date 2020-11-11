9788bb6f2645ffcc0c220be4be3b5721e01bff88
Pekarnik <pekgoglo@gmail.com>
2020-11-12 02:21:17 +0300

    К сожалению увидел решение на уроке, но понял свои ошибки

--- Lesson_8/Lesson_8.sql
+++ Lesson_8/Lesson_8.sql
@@ -11,28 +11,36 @@ SELECT p2.gender, COUNT(*)
 	GROUP BY p2.gender;
 -- 4. ѕодсчитать общее количество лайков дес€ти самым молодым пользовател€м (сколько лайков получили 10 самых молодых пользователей).
 
-SELECT SUM(likes) FROM(SELECT COUNT(l.target_id) AS likes
-	FROM 
-		profiles p
-		LEFT JOIN
-		likes l 
-	ON p.user_id = l.target_id 
-	GROUP BY p.user_id
-	ORDER BY p.birthday DESC
-	LIMIT 10) AS count_likes;
+SELECT * FROM target_types tt ;
+SELECT * FROM likes;
+
+SELECT SUM(got_likes) AS total_likes_for_youngest
+  FROM (   
+    SELECT COUNT(target_types.id) AS got_likes 
+      FROM profiles
+        LEFT JOIN likes
+          ON likes.target_id = profiles.user_id
+        LEFT JOIN target_types
+          ON likes.target_type_id = target_types.id
+            AND target_types.name = 'users'
+      GROUP BY profiles.user_id
+      ORDER BY profiles.birthday DESC
+      LIMIT 10
+) AS youngest;
 
 -- 5. Ќайти 10 пользователей, которые про€вл€ют наименьшую активность в использовании социальной сети
-SELECT u.first_name ,u.last_name, COUNT(l.id)+COUNT(p.id)+COUNT(m.id) as activity
-	FROM users u
-	LEFT JOIN 
-		likes l
-			ON u.id = l.user_id
-	LEFT JOIN 
-		posts p
-			ON u.id = p.user_id
-	LEFT JOIN 
-		media m
-			ON u.id = m.user_id
-	GROUP BY u.id
-	ORDER BY activity LIMIT 10;
+SELECT users.id,
+  COUNT(DISTINCT messages.id) + 
+  COUNT(DISTINCT likes.id) + 
+  COUNT(DISTINCT media.id) AS activity 
+  FROM users
+    LEFT JOIN messages 
+      ON users.id = messages.from_user_id
+    LEFT JOIN likes
+      ON users.id = likes.user_id
+    LEFT JOIN media
+      ON users.id = media.user_id
+  GROUP BY users.id
+  ORDER BY activity
+  LIMIT 10;
 
